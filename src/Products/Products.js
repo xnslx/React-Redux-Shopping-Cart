aa0ce02ce733887e0e4f5c1029d0680d770690b5
Xian Li <xnslx89@gmail.com>
2020-06-15 23:22:34 -0500

    modify

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {initialState} from '../Reducer/Reducer';
+import {connect} from 'react-redux';
 
 
-const Products = () => {
-    const {products} = initialState;
-    console.log(products)
+const Products = (props) => {
+    console.log(props)
+    console.log(initialState)
+    const products = initialState;
+    const [state, setState] = useState('')
+    const changeHandler = e => {
+        setState(e.target.value)
+    }
     return (
         <div>
             {products.map(pdct => {
@@ -13,9 +19,9 @@ const Products = () => {
                         <li><strong>Product Name:</strong> {pdct.name}</li>
                         <li><strong>Product Price:</strong> {pdct.price}</li>
                         <li><strong>Product Stock: </strong>{pdct.stock}</li>
-                        <button>-</button>
-                        <span>Number:</span>
-                        <button>+</button>
+                        <button onClick={props.onProductsDecrease}>-</button>
+                        <input type="number" value={pdct.value} onChange={changeHandler}/>
+                        <button onClick={props.onProductsIncrease}>+</button>
                     </ul>
                 )
             })}
@@ -23,4 +29,18 @@ const Products = () => {
     )
 };
 
-export default Products;
+export const mapStateToProps = state => {
+    return {
+        pdts: state.products
+    }
+};
+
+
+export const mapDispatchToProps = dispatch => {
+    return {
+        onProductsIncrease: (prdtId) => dispatch({type:'INCREASE', id: prdtId}),
+        onProductsDecrease: (prdtId) => dispatch({type:'DECREASE', id:prdtId})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
--- src/Reducer/Reducer.js
+++ src/Reducer/Reducer.js
@@ -1,30 +1,46 @@
-export const initialState = {
-    products: [
-        {
-            id: '1',
-            name: 'iPad Mini',
-            price: '$500',
-            stock: '2'
-        },
-        {
-            id: '2',
-            name: 'H&M T-Shirt White',
-            price: '$10.99',
-            stock: '10'
-        },
-        {
-            id: '3',
-            name: 'Charli XCX - Sucker CD',
-            price: '$19.99',
-            stock: '5'
-        }
-    ],
-    totalPrice: ''
-}
+export const initialState = [
+    {
+        id: '1',
+        name: 'iPad Mini',
+        price: 500,
+        stock: 2,
+        value:0
+    },
+    {
+        id: '2',
+        name: 'H&M T-Shirt White',
+        price: 10.99,
+        stock: 10,
+        value:0
+    },
+    {
+        id: '3',
+        name: 'Charli XCX - Sucker CD',
+        price: 19.99,
+        stock: 5,
+        value:0
+    }
+]
+
 
 const reducer = (state= initialState, action) => {
-    return {
-        
+    switch(action.type) {
+        case 'INCREASE':
+            return state.map(product => {
+                if(product.id === action.id && product.value< product.stock) {
+                    return {...product, value: product.value +1}
+                }
+                return product;
+            })
+        case 'DECREASE':
+            return state.map(product => {
+                if(product.id === action.id && product.value >0) {
+                    return {...product, value: product.value -1}
+                }
+                return product;
+            })
+        default:
+            return state;
     }
 };
 
