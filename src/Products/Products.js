7567bf6003ef8730a0d3ae08238b8d42ce576b01
Xian Li <xnslx89@gmail.com>
2020-06-17 22:56:14 -0500

    modify

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,51 +1,59 @@
 import React from 'react';
-import {initialState} from '../Reducer/Reducer';
+
 import {connect} from 'react-redux';
 
 
-const Products = (props) => {
-    console.log(props)
-    console.log(initialState)
-    const {products} = initialState;
+const Products = props => {
+    // console.log(props)
 
+    const additemsHandler = (id)=> {
+        console.log(id)
+      props.onAddItems(id)
+    };
 
-    // const removeItems = (prdtId) => {
-    //     return props.onRemoveItems;
-    // }
-
-    // const addItems = (prdtId) => {
-    //     return props.onAddItems;
-    // }
-    return (
-        <div>
-            {products.map(pdct => {
-                return (
-                    <ul key={pdct.id}>
-                        <li><strong>Product Name:</strong> {pdct.name}</li>
-                        <li><strong>Product Price:</strong> {pdct.price}</li>
-                        <li><strong>Product Stock: </strong>{pdct.stock}</li>
-                        <button onClick={props.onRemoveItems}>-</button>
-                        <span>{props.cts}</span>
-                        <button onClick={props.onAdditems}>+</button>
-                    </ul>
-                )
-            })}
-        </div>
-    )
+    const removeItemsHandler= (id)=> {
+      props.onRemoveItems(id)
+      // console.log(productId);
+    };
+  return (
+    <div>
+      {props.productList.map(pdct => {
+        return (
+          <ul key={pdct.id}>
+            <li>
+              <strong>Product Name:</strong> {pdct.name}
+            </li>
+            <li>
+              <strong>Product Price:</strong> {pdct.price}
+            </li>
+            <li>
+              <strong>Product Stock: </strong>
+              {pdct.stock}
+            </li>
+            <button onClick={() =>removeItemsHandler(pdct.id)}>-</button>
+            <span>{pdct.counter}</span>
+            <button onClick={() =>additemsHandler(pdct.id)}>+</button>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-    return {
-        cts:state.counter
-    }
+  return {
+    productList: state.products
+  };
 };
 
-
 const mapDispatchToProps = dispatch => {
-    return {
-        onAddItems: (prdtId) => dispatch({type:'INCREASE', id: prdtId}),
-        onRemoveItems: (prdtId) => dispatch({type:'DECREASE', id:prdtId})
-    }
-}
+    // console.log(dispatch)
+  return {
+    onAddItems: productId => dispatch({ type: "INCREASE", id: productId }),
+    onRemoveItems: productId => dispatch({ type: "DECREASE", id: productId })
+  };
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(Products);
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(Products);
