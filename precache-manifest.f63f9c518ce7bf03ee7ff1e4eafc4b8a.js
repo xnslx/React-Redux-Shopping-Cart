cfd4eb6f9b2b72ae97a8760c44438277c77cfef9
Xian Li <xnslx89@gmail.com>
2020-06-25 23:07:59 -0500

    Updates

---
+++ precache-manifest.f63f9c518ce7bf03ee7ff1e4eafc4b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9142b99520ae4428232f3c210ddba7",
+    "url": "/React-Redux-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "4928981fcf94f07cdfbf",
+    "url": "/React-Redux-Shopping-Cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05d541a20cca556c7802",
+    "url": "/React-Redux-Shopping-Cart/static/js/2.29dceed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Shopping-Cart/static/js/2.29dceed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4928981fcf94f07cdfbf",
+    "url": "/React-Redux-Shopping-Cart/static/js/main.87c44bdf.chunk.js"
+  },
+  {
+    "revision": "b66ebd5d81ddc655cf4f",
+    "url": "/React-Redux-Shopping-Cart/static/js/runtime-main.e8372f40.js"
+  }
+]);
